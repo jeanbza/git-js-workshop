93312c9950bf0b5555b486f244a8828c9bc1a779
Jean de Klerk <jadekler@gmail.com>
2015-11-19 21:01:20 -0700

    Checkpoint commit - non jsx react

--- 3.react/main.js
+++ 3.react/main.js
@@ -1,5 +1,18 @@
-import lang from 'src/lang'
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-document.getElementById('in').onchange = lang
+var Foo = React.createClass({
+  displayName: "HelloMessage",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      "Hello ",
+      this.props.name
+    );
+  }
+});
+
+
+ReactDOM.render(React.createElement(Foo, { name: "John" }), document.getElementById('content'));
