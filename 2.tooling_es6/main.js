4e2e4dc7a67e4f2785ad13b3058f8bac41ef4eb0
Jean de Klerk <jadekler@gmail.com>
2016-10-09 10:22:27 -0600

    remove iterator

--- 2.tooling_es6/main.js
+++ 2.tooling_es6/main.js
@@ -3,10 +3,10 @@ var whitelist = ['spa', 'por', 'fra', 'eng', 'rus', 'swe', 'afr', 'fin', 'dan', 
 
 document.getElementById('in').onkeydown = function(event) {
   var results = franc.all(event.target.value, {'whitelist': whitelist})
-  
+
   document.getElementById('results').innerHTML = ''
 
-  for (var result of results) {
-    document.getElementById('results').innerHTML += '<div>' + result[0] + ' ' + result[1] + '</div>'
+  for (i = 0; i < results.length; i++) {
+    document.getElementById('results').innerHTML += '<div>' + results[i][0] + ' ' + results[i][1] + '</div>'
   }
 }
