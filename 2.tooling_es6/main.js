8cb4846771517d6203085f173569afe2e60e4da8
Jean de Klerk <jadekler@gmail.com>
2016-10-09 10:21:02 -0600

    Reset 2.tooling/es6 to baseline

--- 2.tooling_es6/main.js
+++ 2.tooling_es6/main.js
@@ -1,3 +1,12 @@
-import lang from 'src/lang'
+// Block scope! Constants = const, variables = let
+var whitelist = ['spa', 'por', 'fra', 'eng', 'rus', 'swe', 'afr', 'fin', 'dan', 'deu', 'nld']
 
-document.getElementById('in').onchange = lang
+document.getElementById('in').onkeydown = function(event) {
+  var results = franc.all(event.target.value, {'whitelist': whitelist})
+  
+  document.getElementById('results').innerHTML = ''
+
+  for (var result of results) {
+    document.getElementById('results').innerHTML += '<div>' + result[0] + ' ' + result[1] + '</div>'
+  }
+}
