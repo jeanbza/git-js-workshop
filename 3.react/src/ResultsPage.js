b8ee90a3f4b15b2076a891a4d5c98d8ab4beaad7
Jean de Klerk <jadekler@gmail.com>
2016-10-17 14:06:56 -0600

    intermediary

--- 3.react/src/ResultsPage.js
+++ 3.react/src/ResultsPage.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react'
 
 const ResultsPage = React.createClass({
+  getInitialState: function() {
+    return {inputData: 'No input yet'}
+  },
+
   render() {
     return (
-      <div>Hello from ResultsPage.js!</div>
+      <div>
+        <input onChange={this.handleChange}/>
+        <div>
+          {this.state.inputData}
+        </div>
+      </div>
     )
+  },
+
+  handleChange(event) {
+    this.setState({inputData: event.target.value})
   }
 })
 
