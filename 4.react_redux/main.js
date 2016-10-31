96b3f1ac5bd5c63172117e1190634a541288f28a
Jean de Klerk <jadekler@gmail.com>
2016-10-31 13:05:04 -0600

    4. - connect state/dispatch to root

--- 4.react_redux/main.js
+++ 4.react_redux/main.js
@@ -3,16 +3,23 @@ import './stylesheets/app.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
-import {Provider} from 'react-redux'
+import {connect, Provider} from 'react-redux'
 
 import rootReducer from './reducers/rootReducer'
 import ResultsPage from './components/ResultsPage'
+
+const mapStateToProps = state => state // We are just going to map state directly - no custom modifications
+const mapDispatchToProps = dispatch => {return {dispatch: dispatch}} // Let's just pass dispatch directly to ResultsPage
+const ConnectedResultsPage = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ResultsPage)
 
 const store = createStore(rootReducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <ResultsPage />
+    <ConnectedResultsPage />
   </Provider>,
   document.getElementById('content')
 )
